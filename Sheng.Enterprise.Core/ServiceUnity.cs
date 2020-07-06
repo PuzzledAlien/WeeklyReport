4d22650c00b2e13f389d10456784b5e595224443
AllenXu <734329218@qq.com>
2020-07-06 14:55:42 +0800

    Update ServiceUnity.cs

--- Sheng.Enterprise.Core/ServiceUnity.cs
+++ Sheng.Enterprise.Core/ServiceUnity.cs
@@ -1,53 +1,16 @@
 using Linkup.Common;
 using Linkup.Data;
-using System;
 
 namespace Sheng.Enterprise.Core
 {
 	public class ServiceUnity
 	{
-		private static readonly ServiceUnity _instance = new ServiceUnity();
+        public static ServiceUnity Instance { get; } = new ServiceUnity();
 
-		private DatabaseWrapper _dataBase = new DatabaseWrapper();
+        public DatabaseWrapper Database { get; set; } = new DatabaseWrapper();
 
-		private LogService _log = LogService.Instance;
+        public LogService Log { get; } = LogService.Instance;
 
-		private ExceptionHandlingService _exceptionHandling = ExceptionHandlingService.Instance;
-
-		public static ServiceUnity Instance
-		{
-			get
-			{
-				return _instance;
-			}
-		}
-
-		public DatabaseWrapper Database
-		{
-			get
-			{
-				return this._dataBase;
-			}
-			set
-			{
-				this._dataBase = value;
-			}
-		}
-
-		public LogService Log
-		{
-			get
-			{
-				return this._log;
-			}
-		}
-
-		public ExceptionHandlingService ExceptionHandling
-		{
-			get
-			{
-				return this._exceptionHandling;
-			}
-		}
-	}
+        public ExceptionHandlingService ExceptionHandling { get; } = ExceptionHandlingService.Instance;
+    }
 }
